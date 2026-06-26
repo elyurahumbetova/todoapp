2ed5f7bffa1ea1f63669347d7d441ee977579199
Elyura <hhumbetovaa@gmail.com>
2026-06-26 14:06:28 +0400

    Initial Commit

---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Elyura on 26.06.26.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
